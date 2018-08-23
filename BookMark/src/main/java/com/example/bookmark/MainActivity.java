ee474ae77896128a567ac614818798b48b10996c
yaowang <appdev_yw@163.com>
2018-08-23 17:27:15 +0800

    书签模块布局

--- BookMark/src/main/java/com/example/bookmark/MainActivity.java
+++ BookMark/src/main/java/com/example/bookmark/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.bookmark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                //跳出添加书签对话框
+
+                break;
+
+            case R.id.action_search:
+                //跳出书签列表对话框
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
